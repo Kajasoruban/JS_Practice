1b6e12c783349686d765051ab2ec83c760d8e32f
Kajasoruban <kajan.bluwin@gmail.com>
2023-10-16 22:21:42 +0530

    day1 finished

---
+++ script.js
@@ -0,0 +1,60 @@
+// comments can make code readable
+
+// Welcome to learn Javascript
+
+/*comments can make code readable,
+ easy to reuse and informative*/
+
+//  4.Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let a ="kajan";
+let b = 1>0;
+let c;
+let d = null;
+
+// 5.Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+// 6.Declare four variables without assigning values
+
+let e, f, g, h;
+
+// 7.Declare four variables with assigned values
+
+let i = 1;
+let j = 2;
+let k = 3;
+let l = 4;
+
+// 8.Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let fn="Muralitharan";
+let ln="Kajasoruban";
+let ms="Single";
+let cn="SriLanka";
+let ag=21;
+
+// 9.Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let fn2="Muralitharan", ln2="Kajasoruban", ms2="Single", cn2="SriLanka", ag2=21;
+
+// 10.Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 21;
+let yourAge = 25;
+
+console.log(myAge,yourAge);
+
+
+
+
+
+
+
+
+
+

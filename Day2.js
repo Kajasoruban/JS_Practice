246118a6788cb1eb80744d4363399c9e95275ab8
Kajasoruban <kajan.bluwin@gmail.com>
2023-10-17 06:49:48 +0530

    Day2

---
+++ Day2.js
@@ -0,0 +1,107 @@
+//1.Declare a variable named challenge and assign it to an initial value 'learn Javascript'.
+
+let challenge = "learn Javascript";
+
+//2.Print the string on the browser console using console.log()
+
+console.log(challenge);
+
+//3.Print the length of the string on the browser console using console.log()
+
+console.log(challenge.length);
+
+//4.Change all the string characters to capital letters using toUpperCase() method
+
+console.log(challenge.toUpperCase());
+
+//5.Change all the string characters to lowercase letters using toLowerCase() method
+
+console.log(challenge.toLowerCase());
+
+//6.Cut (slice) out the first word of the string using substr() or substring() method
+
+console.log(challenge.substring(6));
+
+//7.Slice out the phrase Days Of JavaScript from learn Javascript.
+
+
+
+// 8.Check if the string contains a word Script using includes() method
+
+
+
+// 9.Split the string into an array using split() method
+
+
+
+// 10.Split the string learn Javascript at the space using split() method
+
+
+
+// 11.'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+
+
+// 12.Change learn Javascript to 30 Days Of Python using replace() method.
+
+
+
+// 13.What is character at index 15 in 'learn Javascript' string? Use charAt() method.
+
+
+
+// 14.What is the character code of J in 'learn Javascript' string using charCodeAt()
+
+
+
+// 15.Use indexOf to determine the position of the first occurrence of a in learn Javascript
+
+
+
+// 16.Use lastIndexOf to determine the position of the last occurrence of a in learn Javascript.
+
+
+
+// 17.Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+
+
+// 18.Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+
+
+// 19.Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+
+
+// 20.Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' learn Javascript '.
+
+
+
+// 21.Use startsWith() method with the string learn Javascript and make the result true
+
+
+
+// 22.Use endsWith() method with the string learn Javascript and make the result true
+
+
+
+// 23.Use match() method to find all the a’s in learn Javascript
+
+
+
+// 24.Use concat() and merge '30 Days of' and 'JavaScript' to a single string, 'learn Javascript'
+
+
+
+// 25.Use repeat() method to print learn Javascript 2 times
+
+
+
+
+
+
+
+
+
+

4d30acc0d5e27dae76414ff92dcfbf05bf947c18
Kajasoruban <kajan.bluwin@gmail.com>
2023-10-31 16:06:14 +0530

    OOP add

---
+++ OOP.js
@@ -0,0 +1,24 @@
+class Person{
+    Id;
+    Name;
+    age;
+    getage(){
+        return this.age;
+    }
+    setage(age){
+        this.age=age;
+    }
+
+}
+let person=new Person();
+
+person.setage(21);
+
+console.log(person);
+
+class Calculator{
+    
+
+
+
+}
